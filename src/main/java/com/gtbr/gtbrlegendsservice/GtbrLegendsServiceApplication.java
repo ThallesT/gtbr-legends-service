fb7b8317f99e293d02578bc9dd1a023dc74b3697
Thalles.tavares <thallestavares.bsi@gmail.com>
2023-03-21 22:00:55 -0300

    initial commit

---
+++ src/main/java/com/gtbr/gtbrlegendsservice/GtbrLegendsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gtbr.gtbrlegendsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtbrLegendsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtbrLegendsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtbr/gtbrlegendsservice/GtbrLegendsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtbr.gtbrlegendsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtbrLegendsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
